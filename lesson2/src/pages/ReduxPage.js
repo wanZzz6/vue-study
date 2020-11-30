5a4a0b48eabd4f9c21db60a3bd3b88e2fe7e6656
wanzheng <1343837706@qq.com>
2020-11-30 22:56:24 +0800

    加中间件实现异步action

--- lesson2/src/pages/ReduxPage.js
+++ lesson2/src/pages/ReduxPage.js
@@ -20,12 +20,25 @@ export default class ReduxPage extends Component {
     store.dispatch({ type: "ADD" });
   };
 
+  asyAdd = () => {
+    // setTimeout(() => {
+    //   store.dispatch({type: "ADD"});
+    // }, 1000);
+
+    store.dispatch((dispatch, getState) => {
+      setTimeout(() => {
+        dispatch({ type: "ADD" });
+      }, 1000);
+    });
+  };
+
   render() {
     return (
       <div>
         <h3>ReduxPage</h3>
         <p>{store.getState()}</p>
         <button onClick={this.add}>add</button>
+        <button onClick={this.asyAdd}>asyAdd</button>
       </div>
     );
   }
