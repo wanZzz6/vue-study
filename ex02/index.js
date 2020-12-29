3cb8aa430460890c47c604abc20a922228431de8
wanzheng <1343837706@qq.com>
2020-12-29 09:24:31 +0800

    ex02

---
+++ ex02/index.js
@@ -0,0 +1,17 @@
+// 暗号 排序
+module.exports.compose = (middlewares) => {
+	return function () {
+		return dispatch(0)
+		function dispatch(i) {
+			let fn = middlewares[i]
+			if (!fn) {
+				return Promise.resolve()
+			}
+			return Promise.resolve(
+				fn(function next() {
+					return dispatch(i + 1)
+				})
+			)
+		}
+	}
+}
