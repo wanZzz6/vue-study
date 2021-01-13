e22e986cee484761eddf9264bcc1e3af30e68b3b
wanzheng <1343837706@qq.com>
2021-01-14 07:29:04 +0800

    ex07

---
+++ ex07/index.js
@@ -0,0 +1,21 @@
+const fs = require('fs')
+module.exports.createLoader = (config) => {
+	const loader = (scanFolder, cb) => {
+		const files = fs.readdirSync(scanFolder)
+		files.forEach((filename) => {
+			filename = filename.replace('.js', '')
+			const file = require(scanFolder + '/' + filename)
+			cb(filename, file)
+		})
+	}
+
+	return {
+		initFunction: (scanFolder) => {
+			const ret = {}
+			loader(scanFolder, (filename, createFun) => {
+				ret[filename] = createFun(config)
+			})
+			return ret
+		},
+	}
+}
