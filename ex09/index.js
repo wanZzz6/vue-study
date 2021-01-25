57809c558ffb846e67c22c1517e108264f48d3ff
wanzheng <1343837706@qq.com>
2021-01-25 22:30:10 +0800

    ex09

---
+++ ex09/index.js
@@ -0,0 +1,14 @@
+module.exports.brackets = (target, property) => {
+	const old = target.prototype[property]
+	target.prototype[property] = (msg) => {
+		msg = `[${msg}]`
+		return old(msg)
+	}
+}
+module.exports.sender = (name) => (target, property) => {
+	const old = target.prototype[property]
+	target.prototype[property] = (msg) => {
+		msg = `${name} : ${msg}`
+		return old(msg)
+	}
+}
